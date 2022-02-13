98845b9b1b9f169cf0f6996fe1098af1aa5f1760
genzabro <99583823+genzabro@users.noreply.github.com>
2022-02-14 08:38:47 +0900

    Initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("fv");
+
+  const data = [...Array(2000).keys()];
+  console.log(data);
+  data.forEach(() => {
+    console.log("aaaaaa");
+  });
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
